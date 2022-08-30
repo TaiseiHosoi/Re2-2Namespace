3db46703cfe5d87a8ed998ca5e8aa51351734590
suga <k021g1264@g.neec.ac.jp>
2022-08-31 02:04:45 +0900

    マウスカーソルを標準

--- Player/Player.cpp
+++ Player/Player.cpp
@@ -138,6 +138,53 @@ void Player::Update(const ViewProjection& viewProjection) {
 
 
 #pragma endregion
+
+#pragma region マウスカーソルのスクリーン座標からワールド座標を取得して3Dレティクル配置
+	POINT mousePosition;
+	//マウス座標(スクリーン座標)を取得
+	GetCursorPos(&mousePosition);
+
+	//クライアントエリア座標に変換する
+	HWND hwnd = WinApp::GetInstance()->GetHwnd();
+	ScreenToClient(hwnd, &mousePosition);
+
+	sprite2DReticle_->SetPosition(Vector2(mousePosition.x, mousePosition.y));
+
+	Matrix4 matVPV = viewProjection.matView * viewProjection.matProjection * Viewport;
+
+	Matrix4 matInverceVPV = MathUtility::Matrix4Inverse(matVPV);
+
+	//スクリーン座標
+	Vector3 posNear = Vector3(mousePosition.x, mousePosition.y, 0);
+	Vector3 posFar = Vector3(mousePosition.x, mousePosition.y, 1);
+
+	//マウスのレイ方向
+	Vector3 mouseDirection = posFar - posNear;
+	mouseDirection = MathUtility::Vector3Normalize(mouseDirection);
+
+	//カメラから標準オブジェク距離
+	const float kDistanceTestObject = 50.0f;
+	worldTransform3DReticle_.translation_ = Affin::AddVector(posNear, mouseDirection * kDistanceTestObject);
+
+	//行列更新
+	Affin::AffinUpdate(worldTransform3DReticle_);
+	worldTransform3DReticle_.TransferMatrix();
+
+	DebugText::GetInstance()->SetPos(20, 200);
+	DebugText::GetInstance()->Printf(
+		"Mouse ScreenPos:(%d,%d)", mousePosition.x, mousePosition.y);
+	DebugText::GetInstance()->SetPos(20, 220);
+	DebugText::GetInstance()->Printf("Near:(%f,%f,%f)",posNear.x,posNear.y,posNear.z);
+	DebugText::GetInstance()->SetPos(20, 240);
+	DebugText::GetInstance()->Printf("Far:(%f,%f,%f)",posFar.x,posFar.y,posFar.z);
+	DebugText::GetInstance()->SetPos(20, 260);
+	DebugText::GetInstance()->Printf(
+	"MouseObject:(%f,%f,%f)",
+		worldTransform3DReticle_.translation_.x, 
+		worldTransform3DReticle_.translation_.y,
+		worldTransform3DReticle_.translation_.z);
+	
+#pragma endregion
 }
 
 void Player::Draw(ViewProjection viewProjection) {
